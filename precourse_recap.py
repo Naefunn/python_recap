bdfc8e9715c7a125021af51c1985fb48562bce05
Naefunn <nathan.d.foster1@gmail.com>
2022-06-20 17:26:39 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+name_1 = "nathan"
+name_2 = "Joe"
+age_1 = 30
+age_2 = 25
+
+new_name_1 = name_1.capitalize()
+
+combined_age = age_1 + age_2 
+
+print(new_name_1 + " and " + name_2 + " have a combined age of " + str(combined_age))
